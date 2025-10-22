29905f02d3ef27548147e3471763ebf35982a02c
Daniel Jaramillo Bustamante <daniel.jaramillo@xpertgroup.co>
2025-10-22 14:00:26 -0500

    Actualización de estructura de directorios

--- bat/iniciar.bat
+++ bat/iniciar.bat
@@ -2,19 +2,19 @@
 echo ===============================
 echo Configurando el proyecto Python...
 
-attrib +h +s "..\src" /s /d
-attrib +h +s "..\Controllers" /s /d
-attrib +h +s "..\python-3.12.5-emb.zip" /s /d
-attrib +h +s "..\python-3.12.5-emb" /s /d
-attrib +h +s "..\Utils" /s /d
-attrib +h +s "..\.gitignore"
-attrib +h +s "..\requirements.txt"
+attrib -h -s "..\src" /s /d
+attrib -h -s "..\Controllers" /s /d
+attrib -h -s "..\python-3.12.5-emb.zip" /s /d
+attrib -h -s "..\python-3.12.5-emb" /s /d
+attrib -h -s "..\Utils" /s /d
+attrib -h -s "..\.gitignore"
+attrib -h -s "..\requirements.txt"
 
 cd "..\python-3.12.5-emb"
 
 echo Instalando paquetes necesarios ...
 
-.\python -m pip install -r ..\requirements.txt --upgrade
+:: .\python -m pip install -r ..\requirements.txt --upgrade
 
 echo Instalacion finalizada cierre la ventana actual.
 pause
