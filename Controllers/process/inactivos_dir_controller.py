4aa5c4ca36124aa504a8cef21e6f46ae07feee01
Daniel Jaramillo Bustamante <daniel.jaramillo@xpertgroup.co>
2025-11-12 11:00:09 -0500

    Actualización final proceso información completa

--- Controllers/process/inactivos_dir_controller.py
+++ Controllers/process/inactivos_dir_controller.py
@@ -47,7 +47,7 @@ class ProcesoDirectaInactivos:
             nom_insumo=self.cfg("maestra_inactivos_dir", "nom_base"),
             nom_hoja=self.cfg("maestra_inactivos_dir", "nom_hoja"),
             cols=COLS_BASE_INACTIVOS,
-            modo_pruebas=True,
+            # modo_pruebas=True,
         )
 
         df_maes_inac = tf.renombrar_columnas_con_diccionario(
